d38de95446184d349c69e60def273b687481afe9
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-06-09 18:00:01 +0100

    teacher dashboard

--- my-react-app/src/Teacher-page/Teacher.js
+++ my-react-app/src/Teacher-page/Teacher.js
@@ -14,6 +14,7 @@ function Teacher(props) {
     const [currentMonth, setCurrentMonth] = useState(today.getMonth());
     const [currentYear, setCurrentYear] = useState(today.getFullYear());
     const [teacherInfo, setTeacherInfo] = useState({})
+    const [studentInfo, setStudentInfo] = useState({})
 
     const DaysInMonth = (month, year) => {
         return new Date(year, month + 1, 0).getDate();
@@ -53,18 +54,33 @@ function Teacher(props) {
       
     }, [])
     
+    // fetch students information as teacher logs in
+    useEffect(() => {
+      const fetchStudentInfo = async () => {
+        try {
+            // fetches fname, lname of a particular class
+            response = await fetch(`/student-info/${teacherInfo["class-assigned"]}`)
+            result = await response.json()
+            setStudentInfo(result)
+        } catch (error) {
+            console.log(`Error: ${error}`)
+        }
+        fetchStudentInfo();
+      }
+    }, [])
+    
+
     
     return(
         <div class="container-fluid">
             <div className=" row teacher-container">
                 <div className="col- 6 teacher-info">
                     <div className="infor-teach">
-                        {Object.keys(teacherInfo).forEach((key, index) => {
-                            <p key={index}>{key} : {teacherInfo[key]}</p>
+                        {/* {display teachers information} */}
+                        {Object.keys(teacherInfo).map((key, i) => {
+                            <p key={i}>{key} : {teacherInfo[key]}</p>
                         })}
                     </div>
-                    <p class="list-of-info">
-                    </p>
                 </div>
                 <div class="col-6 calender-comp">
                     <button onClick={handlePrevMonth}> <FontAwesomeIcon icon={faCircleChevronLeft} /></button>
@@ -100,6 +116,37 @@ function Teacher(props) {
             </div>
             
         </div>
+        <div className="form-container">
+            <form method="post" onsubmit={handleSubmit}>
+            <table>
+                <thead>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Present</th>
+                    <th>Absent</th>
+                    <th>Date</th>
+                </thead>
+                {/* {from the array of all students in a class display the names
+                    and mark the attendance state
+                }*/}
+                <tbody>
+                   
+                {(studentInfo).map((el, i) => {
+                    // studentinfo is an array of objects
+                     <tr>
+                        <td key={i}> <input type="text" defaultValue={el[firstname]} name="firstname" id="firstname" /></td>
+                        <td key={i}><input type="text" defaultValue={el[lastname]} name="lastname" id="lastname" /></td>
+                        <td key={i}><input type="radio" name="present" id="present" /></td>
+                        <td key={i}><input type="radio" name="absent" id="absent" /></td>
+                        <td key={i}><input type="date" defaultValue={currentDate} name="todaydate" id="todaydate" /></td>
+                    </tr>
+                })}
+                    
+                </tbody>
+            </table>
+            </form>
+        </div>
+
     </div>
     )
 
