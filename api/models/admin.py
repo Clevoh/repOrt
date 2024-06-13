31a5639167f735b1901dc58a3e672bb4eda74a9c
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-06-13 18:06:52 +0100

    update

--- api/models/admin.py
+++ api/models/admin.py
@@ -3,6 +3,20 @@ from . import db
 
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(20), nullable=False)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname =  db.Column(db.String(80), nullable=False)
+    gender = db.Column(db.String(80), nullable=False)
+    sclass = db.Column(db.String(80), nullable=False)
+    dateofbirth = db.Column(db.String(80), nullable=False)
+    religion = db.Column(db.String(80), nullable=False)
+    address = db.Column(db.String(80), nullable=False)
+    bloodgroup = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f'<Teacher {self.name}>'
+
+    def hash_password(self, password):
+        self.hash_password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.hash_password, password)
